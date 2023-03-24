e981fd536baad56a34e4577b7b8c387fbfbb402e
Dan97O <dan97-oddo@live.it>
2023-03-24 16:29:49 +0100

    add function feed and post element

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,44 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+/* 
+Descrizione
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+Milestone 1
+ Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+id del post, numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Non è necessario creare date casuali Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+*/
+
+//selezione il container nell'html
+const containerEl = document.querySelector("#container");
+
+// Ciclo ogni oggetto posts e appendo al container postElement
+function feed(posts, containerEl) {
+    posts.forEach(post => {
+        const postEl = postElement(post);
+        containerEl.append(postEl);
+    });
+}
+//richiamo la funzione feed
+feed(posts, containerEl);
+
+//creo l'elemento Div gli aggiungo la classe post 
+function postElement(post) {
+    const divElement = document.createElement("div");
+    divElement.classList.add("post");
+    divElement.innerHTML = markup;
+    return divElement;
+}
+
+
