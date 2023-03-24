5a0b84554b443f459e0927563eef7878bc4bfc7d
Dan97O <dan97-oddo@live.it>
2023-03-24 16:42:58 +0100

    add markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,11 +88,47 @@ function feed(posts, containerEl) {
 feed(posts, containerEl);
 
 //creo l'elemento Div gli aggiungo la classe post 
+//creo const markup e gli collego la funzione createMarkup
 function postElement(post) {
+    const markup = createMarkup(post);
     const divElement = document.createElement("div");
     divElement.classList.add("post");
     divElement.innerHTML = markup;
     return divElement;
 }
 
-
+//creo il markup
+function createMarkup(post) {
+    const markup = `
+    <div class="post__header">
+        <div class="post-meta">
+            <div class="post-meta__icon">
+                <img class="profile-pic" src=${post.author.image} alt="${post.author.name}">
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${post.author.name}</div>
+                <div class="post-meta__time">${post.created}</div>
+            </div>
+        </div>
+    </div>
+    <div class="post__text">${post.content}</div>
+    <div class="post__image">
+        <img src=${post.media} alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+            </div>
+        </div>
+    </div>`
+    return markup;
+  }
+  
+  
